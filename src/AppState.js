c53ab4d1157c3f69fb4fe14d400673ae3198288c
Cathella <ncathie214@gmail.com>
2021-12-05 09:07:00 +0300

    Add register user functionality

--- src/AppState.js
+++ src/AppState.js
@@ -2,12 +2,48 @@ import React, { useReducer, useContext } from "react";
 
 // Initial state
 const initialState = {
-  url: 'http://homework-buddy.herokuapp.com'
+  url: 'http://homework-buddy.herokuapp.com',
+  token: null,
+  username: null
 }
 
 // Reducer
 const reducer = (state, action) => {
   switch (action.type) {
+    case 'register':
+      fetch(state.url + '/users/', {
+        method: 'POST',
+        headers: {
+          "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(action.payload)
+      })
+      .then(response => response.json())
+      .then(user => {
+        return {
+          ...state,
+          token: user.token,
+          username: user.username
+        }
+      })
+      break
+    case 'login':
+      fetch(state.url + '/login/', {
+        method: 'POST',
+        headers: {
+          "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(action.payload)
+      })
+      .then(response => response.json())
+      .then(user => {
+        return {
+          ...state,
+          token: user.token,
+          username: user.username
+        }
+      })
+      break
     default:
       return state
   }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { useAppState } from '../AppState';
 
 const Register = (props) => {
-  return <h1>Register</h1>
+  const [formData, setFormData] = React.useState({
+    username: "",
+    password: ""
+  });
+
+  const { dispatch } = useAppState();
+
+  const actions = {
+    register: {
+      type: 'register',
+      payload: formData
+    },
+  }
+
+  const handleChange = (event) => {
+    setFormData({...formData, [event.target.name]: event.target.value})
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(actions.register);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Username" name="username" value={formData.username} onChange={handleChange} />
+        <input type="password" placeholder="Password" name="password" value={formData.password} onChange={handleChange} />
+        <input type="submit" value="Register" />
+      </form>
+    </div>
+  )
+
 }
 
 export default Register;
